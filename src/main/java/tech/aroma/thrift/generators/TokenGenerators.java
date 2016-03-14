0a513d100fd5316200ae77d4fe6d18590ea87968
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-03-13 20:09:02 -0700

    Adding TokenGenerators

---
+++ src/main/java/tech/aroma/thrift/generators/TokenGenerators.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2015 Aroma Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.aroma.thrift.generators;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.aroma.thrift.authentication.ApplicationToken;
+import tech.aroma.thrift.authentication.AuthenticationToken;
+import tech.aroma.thrift.authentication.TokenType;
+import tech.aroma.thrift.authentication.UserToken;
+import tech.sirwellington.alchemy.annotations.access.NonInstantiable;
+import tech.sirwellington.alchemy.generator.AlchemyGenerator;
+
+import static tech.sirwellington.alchemy.generator.AlchemyGenerator.one;
+import static tech.sirwellington.alchemy.generator.EnumGenerators.enumValueOf;
+import static tech.sirwellington.alchemy.generator.StringGenerators.alphabeticString;
+import static tech.sirwellington.alchemy.generator.StringGenerators.uuids;
+import static tech.sirwellington.alchemy.generator.TimeGenerators.futureInstants;
+
+/**
+ * {@linkplain AlchemyGenerator Alchemy Generators} for {@link AuthenticationToken}, {@link UserToken}, and
+ * {@link ApplicationToken} types.
+ *
+ * @author SirWellington
+ */
+@NonInstantiable
+public final class TokenGenerators
+{
+    
+    private final static Logger LOG = LoggerFactory.getLogger(TokenGenerators.class);
+    
+    TokenGenerators() throws IllegalAccessException
+    {
+        throw new IllegalAccessException("cannot instatiate");
+    }
+    
+    public static AlchemyGenerator<AuthenticationToken> authenticationTokens()
+    {
+        return () ->
+        {
+            return new AuthenticationToken()
+                .setTokenId(one(uuids))
+                .setOwnerId(one(uuids))
+                .setOwnerName(one(alphabeticString()))
+                .setOrganizationId(one(uuids))
+                .setOrganizationName(one(alphabeticString()))
+                .setTokenType(enumValueOf(TokenType.class).get())
+                .setTimeOfExpiration(futureInstants().get().toEpochMilli());
+        };
+    }
+    
+    public static AlchemyGenerator<ApplicationToken> applicationTokens()
+    {
+        return () ->
+        {
+            return new ApplicationToken()
+                .setApplicationId(one(uuids))
+                .setApplicationName(one(alphabeticString()))
+                .setOrganization(one(alphabeticString()))
+                .setTokenId(one(uuids))
+                .setTimeOfExpiration(futureInstants().get().toEpochMilli());
+        };
+    }
+    
+    public static AlchemyGenerator<UserToken> userTokens()
+    {
+        return () ->
+        {
+            return new UserToken()
+                .setUserId(one(uuids))
+                .setTokenId(one(uuids))
+                .setOrganization(one(alphabeticString()))
+                .setTimeOfExpiration(futureInstants().get().toEpochMilli());
+        };
+    }
+    
+}
---
+++ src/test/java/tech/aroma/thrift/generators/TokenGeneratorsTest.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2016 RedRoma.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.aroma.thrift.generators;
+
+import java.time.Instant;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import tech.aroma.thrift.authentication.ApplicationToken;
+import tech.aroma.thrift.authentication.AuthenticationToken;
+import tech.aroma.thrift.authentication.UserToken;
+import tech.sirwellington.alchemy.generator.AlchemyGenerator;
+import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner;
+import tech.sirwellington.alchemy.test.junit.runners.DontRepeat;
+import tech.sirwellington.alchemy.test.junit.runners.GenerateInteger;
+import tech.sirwellington.alchemy.test.junit.runners.Repeat;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static sir.wellington.alchemy.collections.sets.Sets.toSet;
+import static tech.sirwellington.alchemy.arguments.Arguments.checkThat;
+import static tech.sirwellington.alchemy.arguments.assertions.StringAssertions.nonEmptyString;
+import static tech.sirwellington.alchemy.arguments.assertions.StringAssertions.validUUID;
+import static tech.sirwellington.alchemy.arguments.assertions.TimeAssertions.inTheFuture;
+import static tech.sirwellington.alchemy.generator.CollectionGenerators.listOf;
+import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThrows;
+import static tech.sirwellington.alchemy.test.junit.runners.GenerateInteger.Type.RANGE;
+
+/**
+ *
+ * @author SirWellington
+ */
+@Repeat(100)
+@RunWith(AlchemyTestRunner.class)
+public class TokenGeneratorsTest 
+{
+
+    @GenerateInteger(value = RANGE, min = 5, max = 50)
+    private int size;
+    
+    @Before
+    public void setUp() throws Exception
+    {
+        
+    }
+
+    @DontRepeat
+    @Test
+    public void testCannotInstatiate()
+    {
+        assertThrows(() -> new TokenGenerators());
+    }
+    
+    @Test
+    public void testAuthenticationTokens()
+    {
+        AlchemyGenerator<AuthenticationToken> generator = TokenGenerators.authenticationTokens();
+        assertThat(generator, notNullValue());
+        
+        AuthenticationToken token = generator.get();
+        assertToken(token);
+        
+        List<AuthenticationToken> tokens = listOf(generator, size);
+        assertThat(tokens.size(), is(size));
+        assertThat(toSet(tokens).size(), is(size));
+        tokens.forEach(this::assertToken);
+    }
+
+    @Test
+    public void testApplicationTokens()
+    {
+        AlchemyGenerator<ApplicationToken> generator = TokenGenerators.applicationTokens();
+        assertThat(generator, notNullValue());
+        
+        ApplicationToken token = generator.get();
+        assertToken(token);
+        
+        List<ApplicationToken> tokens = listOf(generator, size);
+        assertThat(toSet(tokens).size(), is(size));
+        tokens.forEach(this::assertToken);
+    }
+
+    @Test
+    public void testUserTokens()
+    {
+        AlchemyGenerator<UserToken> generator = TokenGenerators.userTokens();
+        assertThat(generator, notNullValue());
+        
+        UserToken token = generator.get();
+        assertToken(token);
+        
+        List<UserToken> tokens = listOf(generator, size);
+        assertThat(toSet(tokens).size(), is(size));
+        tokens.forEach(this::assertToken);
+    }
+
+    private void assertToken(AuthenticationToken token)
+    {
+        assertThat(token, notNullValue());
+        checkThat(token.tokenId, token.ownerId, token.organizationId)
+            .are(validUUID());
+        
+        checkThat(token.ownerName, token.organizationName)
+            .are(nonEmptyString());
+        
+        checkThat(Instant.ofEpochMilli(token.timeOfExpiration))
+            .is(inTheFuture());
+    }
+
+    private void assertToken(ApplicationToken token)
+    {
+        assertThat(token, notNullValue());
+        
+        checkThat(token.tokenId, token.applicationId)
+            .are(validUUID());
+        
+        checkThat(Instant.ofEpochMilli(token.timeOfExpiration))
+            .is(inTheFuture());
+
+    }
+
+    private void assertToken(UserToken token)
+    {
+        assertThat(token, notNullValue());
+        
+        checkThat(token.tokenId, token.userId)
+            .are(validUUID());
+        
+        checkThat(Instant.ofEpochMilli(token.timeOfExpiration))
+            .is(inTheFuture());
+
+    }
+
+}
