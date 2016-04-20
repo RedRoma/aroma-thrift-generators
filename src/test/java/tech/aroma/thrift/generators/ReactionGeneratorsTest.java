450b161a60608d1f6c1771726c00e947467b9ee5
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-04-20 12:47:32 -0700

    Unit test additions: ReactionGeneratorsTest

---
+++ src/test/java/tech/aroma/thrift/generators/ReactionGeneratorsTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2016 RedRoma.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.aroma.thrift.generators;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import tech.aroma.thrift.reactions.AromaMatcher;
+import tech.sirwellington.alchemy.generator.AlchemyGenerator;
+import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner;
+import tech.sirwellington.alchemy.test.junit.runners.Repeat;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author SirWellington
+ */
+@Repeat(50)
+@RunWith(AlchemyTestRunner.class)
+public class ReactionGeneratorsTest 
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+        
+        setupData();
+        setupMocks();
+    }
+
+
+    private void setupData() throws Exception
+    {
+        
+    }
+
+    private void setupMocks() throws Exception
+    {
+        
+    }
+
+    @Test
+    public void testMatchers()
+    {
+        AlchemyGenerator<AromaMatcher> instance = ReactionGenerators.matchers();
+        assertThat(instance, notNullValue());
+        
+        AromaMatcher matcher = instance.get();
+        assertThat(matcher, notNullValue());
+        assertThat(matcher.isSet(), is(true));
+    }
+
+}
