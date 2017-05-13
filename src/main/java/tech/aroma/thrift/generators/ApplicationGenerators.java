09c49fd4ba2b60965bee644bfeacd3734535b4e7
Wellington Moreno <jwellington.moreno@gmail.com>
2017-05-13 00:39:11 -0700

    Improves ApplicationGenerators

--- src/main/java/tech/aroma/thrift/generators/ApplicationGenerators.java
+++ src/main/java/tech/aroma/thrift/generators/ApplicationGenerators.java
@@ -1,18 +1,18 @@
- /*
-  * Copyright 2017 RedRoma, Inc.
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *      http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+/*
+ * Copyright 2017 RedRoma, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package tech.aroma.thrift.generators;
 
@@ -31,61 +31,68 @@ import static tech.sirwellington.alchemy.generator.NumberGenerators.integers;
 import static tech.sirwellington.alchemy.generator.NumberGenerators.positiveLongs;
 import static tech.sirwellington.alchemy.generator.PeopleGenerators.names;
 import static tech.sirwellington.alchemy.generator.StringGenerators.uuids;
+import static tech.sirwellington.alchemy.generator.TimeGenerators.futureInstants;
 import static tech.sirwellington.alchemy.generator.TimeGenerators.pastInstants;
 
 /**
  * {@linkplain AlchemyGenerator Alchemy Generators} for {@link Application} types.
- * 
+ *
  * @author SirWellington
  */
 @NonInstantiable
 public final class ApplicationGenerators
 {
-    
+
     private final static Logger LOG = LoggerFactory.getLogger(ApplicationGenerators.class);
-    
+
     ApplicationGenerators() throws IllegalAccessException
     {
         throw new IllegalAccessException("cannot instantiate");
     }
-    
+
     public static AlchemyGenerator<Long> pastTimes()
     {
         return () -> pastInstants().get().toEpochMilli();
     }
-    
+
+    public static AlchemyGenerator<Long> futureTimes()
+    {
+        return () -> futureInstants().get().toEpochMilli();
+    }
+
     public static AlchemyGenerator<ProgrammingLanguage> languages()
     {
         return EnumGenerators.enumValueOf(ProgrammingLanguage.class);
     }
-    
+
     public static AlchemyGenerator<Application> applications()
     {
         return () ->
         {
             int numberOfOwners = one(integers(1, 4));
             int numberOfFollowers = one(integers(0, 1000));
-            
+
             return new Application()
-                .setApplicationId(one(uuids))
-                .setName(names().get())
-                .setApplicationIconMediaId(one(uuids))
-                .setProgrammingLanguage(one(languages()))
-                .setFollowers(toSet(listOf(uuids, numberOfFollowers)))
-                .setOwners(toSet(listOf(uuids, numberOfOwners)))
-                .setTotalMessagesSent(one(positiveLongs()))
-                .setTimeOfProvisioning(one(pastTimes()));
-            
+                    .setApplicationId(one(uuids))
+                    .setName(names().get())
+                    .setApplicationIconMediaId(one(uuids))
+                    .setProgrammingLanguage(one(languages()))
+                    .setFollowers(toSet(listOf(uuids, numberOfFollowers)))
+                    .setOwners(toSet(listOf(uuids, numberOfOwners)))
+                    .setTotalMessagesSent(one(positiveLongs()))
+                    .setTimeOfProvisioning(one(pastTimes()))
+                    .setTimeOfTokenExpiration(one(futureTimes()));
+
         };
     }
-    
+
     public static AlchemyGenerator<Application> applicationsWithIcons()
     {
         return () ->
         {
             Application app = applications().get();
             app.setIcon(one(ImageGenerators.appIcons()));
-            
+
             return app;
         };
     }
